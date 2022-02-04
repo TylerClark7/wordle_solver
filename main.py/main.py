607acccbef7dca3614234ec9928a1240e9e84977
Tyler Clark <tjclark599@gmail.com>
2022-02-03 23:47:17 -0600

    added main.py into rep

---
+++ main.py/main.py
@@ -0,0 +1,68 @@
+with open("main.py\words.txt", "r") as file:
+    words = [word.strip("\n") for word in file]
+    wordle_words = [word.lower() for word in words if len(word) == 5]
+
+           
+        
+known_spots = input("Enter known wordle letters using '-' to seperate unknown blanks: ")
+
+
+
+possible_words = []
+
+indexes = []
+ind = 0
+for letter in known_spots:
+    if letter != "-":
+        indexes.append((ind,letter))
+    ind += 1
+
+
+
+for word in wordle_words:
+    word_inds = []
+    for char in word:
+        if (word.index(char),char) in indexes:
+            word_inds.append((word.index(char),char))
+    if word_inds == indexes:
+        print(word)
+
+
+
+
+             
+
+    
+        
+
+#Take a word
+
+#take a string of letters and underscores
+
+#check each letter of the word and see if the letters index matches the index of same letter in string string
+
+
+
+#
+"""          
+for item in indexes:
+    print(item[0])
+"""
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+        
+    
+
+
+            
